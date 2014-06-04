b99e5b09d1beadd4d4564117a68aca3ab1b1403a
Peter Fraenkel <pnf@podsnap.com>
2014-06-04 16:27:30 -0400

    Use instance profile

--- src/girder/aws_script.clj
+++ src/girder/aws_script.clj
@@ -12,6 +12,8 @@
 
 ;(describe-images :owners ["self"])
 ;aws ec2 request-spot-instances --spot-price 0.005 --instance-count 1 --type "one-time" --launch-specification '{"ImageId" : "ami-a048bec8", "InstanceType" : "t1.micro", "Placement" : { "AvailabilityZone" : "us-east-1c"}, "KeyName" : "telekhine", "SecurityGroupIds" : ["sg-149f3c7e"]}'
+; "IamInstanceProfile" : {"Arn" : "arn:aws:iam::633840533036:instance-profile/datomic-aws-peer" }
+
 
 (defn requests [n]
   (let [r (request-spot-instances
@@ -21,11 +23,21 @@
            :launch-specification 
            {:image-id 			"ami-a048bec8"
             :instance-type 		"t1.micro"
-            :placement
-            {:availability-zone 	"us-east-1c"}
+            :placement                  {:availability-zone 	"us-east-1c"}
             :key-name			"telekhine"
-            :security-groups		["launch-wizard-6"]})]
+            :security-groups		["launch-wizard-6"]
+            :iam-instance-profile       {:arn "arn:aws:iam::633840533036:instance-profile/girder-peer"}})]
     (map :spot-instance-request-id  (:spot-instance-requests r))))
+
+(defn monitor-status [rs]
+  (loop [prev nil]
+    
+
+)
+ 
+
+)
+
 
 (defn request-status [rs]
   (let [d (describe-spot-instance-requests :spot-instance-request-ids rs)]
@@ -53,8 +65,11 @@
                             :username "ec2-user"})
        hosts))
 
+(defn ex [sess cmd]
+  (or (ssh/connected? sess) (ssh/connect sess))
+  (ssh/ssh-exec sess cmd "" "" {}))
 
-(defn ex [sess c cmd]
+(defn ex-async [sess cmd c]
   (or (ssh/connected? sess) (ssh/connect sess))
   (go (>! c (ssh/ssh-exec sess cmd "" "" {})))
   c)
@@ -62,9 +77,14 @@
 
 
 ;(map disconnect ss)
-
-
-
 ;(describe-spot-instance-requests :spot-instance-request-ids r)
-
 ; (filter #(= "sir-b7586249" (:spot-instance-request-id %)) (get-in d [:spot-instance-requests]))
+
+#_(def prepare-cmds
+  "cd $HOME/girder
+git pull
+$HOME/bin/lein deps
+$HOME/bin/lein compile
+$HOME/bin/lein run --nTt 10")
+
+(def get-jar )
