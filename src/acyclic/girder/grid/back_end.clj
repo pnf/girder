6f010b8cc360fefb976a713983929eb0405cc4cd
Peter Fraenkel <pnf@podsnap.com>
2014-08-25 12:34:21 -0400

    Add doc strings.

--- src/acyclic/girder/grid/back_end.clj
+++ src/acyclic/girder/grid/back_end.clj
@@ -1,41 +1,36 @@
 (ns acyclic.girder.grid.back-end)
 
 (defprotocol Girder-Backend 
-
   (clean-all [this] "Clear out database, or whatever")
-
-  (clpush [this key queue-type]
-    "Push onto the main key value pub/sub queue, via the channel, which is returned.")
-  (crpop [this key queue-type])
-  (lpush [this key queue-type val])
+  (crpop [this key queue-type] "Return a channel that is fed from the specified queue.")
+  (clpush [this key queue-type]  "Return a channel, writing to which will feed the specified queue.")
+  (lpush [this key queue-type val] "Explicitly push val onto the specified queue.")
   (lpush-many [this key queue-type vals])
   (lpush-and-set [this
                   qkey queue-type qval
-                  vkey val-type vval])
-  (clear-bak [this qkeys-qtypes])
-  (get-members [this key set-type])
-  (get-val [this key val-type])
-  (set-val  [this key val-type val])
-  (qall [this key :queue-type])
+                  vkey val-type vval]
+    "Explicitly push qval onto the specified queue, atomically setting the specified scalar to vval.")
+  (clear-bak [this qkeys-qtypes] "Clear the backups of elements popped from queues.")
+  (get-members [this key set-type] "Return the members from the specified set.")
+  (get-val [this key val-type] "Return the value from a specified key.")
+  (set-val  [this key val-type val] "Set a value from the specified key, returning the old value.  Set nil to erase.")
+  (qall [this key :queue-type] "Fetch, but do not pop, all members of the specified queue.")
   (add-member [this key set-type mem])
   (remove-member [this key set-type mem])
 
-  (kv-listen [this k deb])
-  (kv-publish [this k v])
-  (kv-close [this])
-
-  (srem [this key val])
+  (kv-listen [this k debug-info] "Return a channel that will receive the next publication to the specified key.")
+  (kv-publish [this k v] "Publish a value by the specified key.")
+  (kv-close [this] "Shut down the internal listening process.  Probably don't do this.")
 
   (enqueue-listen
     [this
      queueid reqid 
-     queue-type val-type
+     queue-type state-type
      enqueue-pred
      done-pred
      done-extract
      debug-info]
-    "Places the req on a queue, returning a channel containing updates to the request's state.
+    "Places the request on a queue, returning a channel containing updates to the request's state.
 Internally, the queue and request ids may be turned into specific keys for a queue, a publication
 topic and a state variable.  The two predicates, if specified, determine based on the state, whether
-something should be enqueued and whether the request should be considered complete.")
-  )
+something should be enqueued and whether the request should be considered complete."))
