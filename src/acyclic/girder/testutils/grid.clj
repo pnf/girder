55963c1fef32b3dbdcfb164780bcf7c4a57eede6
Peter Fraenkel <pnf@podsnap.com>
2014-08-28 18:05:52 -0400

    Upgrade grid executable to latest cli and grid api.

--- src/acyclic/girder/testutils/grid.clj
+++ src/acyclic/girder/testutils/grid.clj
@@ -17,28 +17,34 @@
    [nil "--helper MSECS" "Launch helper for the designated pool" :parse-fn #(Integer/parseInt %) :default nil]
    [nil "--jobs N" :default 0 :parse-fn #(Integer/parseInt %)]
    [nil "--jobtimeout SECS" "Time to wait." :parse-fn #(Integer/parseInt %) :default 10]
-   [nil "--reclevel N" "Number of recursions" :parse-fn #(Integer/parseInt %) :default :info]
+   [nil "--reclevel N" "Number of recursions" :parse-fn #(Integer/parseInt %) :default 0]
+   [nil "--numrecjobs N" "Number of jobs to launch at each level of recursion"
+    :parse-fn #(Integer/parseInt %) :default 2]
    [nil "--cleanup"]
    ["-i" "--id NUM" "Some id number" :default 0 :parse-fn #(Integer/parseInt %)]
    [nil "--log LEVEL" "Debug level" :parse-fn keyword :default nil]
    ["-o" "--opts OPTS" "EDN string" :default nil :parse-fn read-string]
    [nil "--repl" "Set when running in REPL, so exit isn't called"]
-   [nil "--hang SECS" "After launching, hang around for this period before exiting automatically."
-    :parse-fn #(Integer/parseInt %) :default 0]
-   [nil "--cmt COMMENT" "Some comment to stick into job request ids" :default (str (rand))]
-   ["-h" "--help"]])
+   [nil "--cmt COMMENT" "Some comment to stick into job request ids" :default (str (rand))]])
 
 (grid/cdefn recbog [msec jobnum reclevel numrecjobs args]
-  (let [_ (debug "here we are in recbog")
-        reqs (map #(recbog msec % (dec reclevel)  numrecjobs args) (range reclevel))
-        _   (debug "recbog asking for" reqs)
-        vs  (grid/requests reqs)]
-    (Thread/sleep msec)
-    (str "RecBog:" grid/*nodeid* ":" jobnum ":" msec ":" reclevel ":" args ":[" (clojure.string/join "," (map str vs)) "]")    ))
+       (debug "here we are in recbog" grid/*nodeid* msec jobnum reclevel numrecjobs args)
+       (if-not (pos? reclevel)
+         (let [res (str jobnum)]
+           (Thread/sleep msec)
+           (debug "recbog" msec jobnum reclevel numrecjobs args "returning" res)
+           res)
+         (let [reqs (map #(vector recbog msec % (dec reclevel) numrecjobs args) (range numrecjobs))
+               vs  (grid/requests reqs)
+               res (str "RecBog:" grid/*nodeid* ":" jobnum ":" msec ":" reclevel ":" args ":[" (clojure.string/join "," (map str vs)) "]")]
+           (debug "recbog" msec jobnum reclevel numrecjobs args "returning" res)
+           (Thread/sleep msec)
+           res
+    )))
 
 (defn doit [opts]
   (acyclic.girder.grid.redis/init! (:host opts) (:port opts))
-  (let [{:keys [reclevel cmt help pool worker distributor host port jobmsecs jobs jobtimeout helper cleanup]} opts]
+  (let [{:keys [numrecjobs reclevel cmt help pool worker distributor host port jobmsecs jobs jobtimeout helper cleanup]} opts]
     {:cleanup
      (when cleanup (grid/cleanup))
      :distributor
@@ -56,7 +62,7 @@
            (grid/launch-worker w pool))))
      :jobs
      (when (and pool jobs (pos? jobs))
-       (let [c (async/map vector (map #(grid/enqueue pool [recbog jobmsecs % reclevel jobs cmt]) (range jobs)))
+       (let [c (async/map vector (map #(grid/enqueue pool [recbog jobmsecs % reclevel numrecjobs cmt]) (range jobs)))
              t (async/timeout (* 1000 jobtimeout))]
          (let [[v ch] (async/alts!! [c t])]
            (or v "timeout"))))}
