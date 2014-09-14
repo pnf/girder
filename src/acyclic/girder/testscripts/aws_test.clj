5c03e5bcdcd2c9b3936142e1fe1e778e4f872961
Peter Fraenkel <pnf@podsnap.com>
2014-09-14 17:45:44 -0400

    Add redis logging.

--- src/acyclic/girder/testscripts/aws_test.clj
+++ src/acyclic/girder/testscripts/aws_test.clj
@@ -7,50 +7,69 @@
         acyclic.awstools.core)
   (:require [clojure.tools.cli :refer [parse-opts]]
             [clojure.core.async :as async :refer [<! >! <!! >!! timeout chan alt!! go close!]]
-            [taoensso.timbre :as timbre]))
+            [taoensso.timbre :as timbre]
+            [taoensso.timbre.appenders.carmine :as car-appender]
+            [clj-ssh.ssh :as ssh]))
 (timbre/refer-timbre)
+
 
 (defn cmd-getjar []
   (str "aws s3 cp s3://dist-ec2/girder.jar ."))
 
-(defn cmds-workers [n pool ip]
+(defn addlog [log]
+  (when log
+    (let [log  (name log)]
+      (str " --log " log))))
+
+(defn cmds-workers [n pool ip & [log]]
   [(cmd-getjar)
    (str "java -cp girder.jar acyclic.girder.testutils.grid --worker " n
-        " --pool " pool " --host " ip " --hang 1000")])
+        " --pool " pool " --host " ip " --hang 1000"
+        (addlog log))])
 
-(defn cmds-dist [pool ip]
+(defn cmds-dist [pool ip & [log]]
     [(cmd-getjar)
      (str "java -cp girder.jar acyclic.girder.testutils.grid --distributor " pool
-          " --host " ip " --hang 1000")])
+          " --host " ip " --hang 1000" (addlog log))])
 
 
-(defn cmd-job [pool ip jobs reclevel]
+(defn cmd-job [pool ip jobs reclevel & [log]]
   (str  "java -cp girder.jar acyclic.girder.testutils.grid --pool " pool 
-        " --host " ip " --jobs " jobs " --reclevel " reclevel))
+        " --host " ip " --jobs " jobs " --reclevel " reclevel
+         (addlog log)))
 
 (defn cmds-redis [] [(str "bin/redis-server")])
 
 
-(slurp-ec2-info "/Users/pnf/dev/girder/EC2DATA.clj")
-
 (def c-listener (start-up-listener))
 
-(def c-nat (bring-up-instances [(:nat-id my-ec2-info)]))
-(def r-nat (<!! c-nat))
+(slurp-ec2-info "EC2DATA.clj")
 
+(def c-nat (bring-up-instances [(:nat-id my-ec2-info)]))
 (def c-redis  (bring-up-spots my-req 1 (cmds-redis) :subnet my-sub-public :itype "m3.medium" :price 0.05 :key "telekhine" :log "debug"))
+(def r-nat (<!! c-nat))
 (def r-redis (<!! c-redis))
+
 (def redis (first (vals r-redis)))
 (def redis-ip (:ip redis))
 (def redis-host (:host redis))
-
 (def s-redis (ssh-session redis-host))
+(ex s-redis "bin/redis-cli flushdb")
 
 
-(def c-dist (bring-up-spots my-req 1 (cmds-dist "pool" redis-ip) :subnet my-sub-private :itype "t1.micro" :price 0.01 :key "girder" :minutes 100))
+(ssh/connect s-redis)
+(ssh/forward-local-port s-redis 8379 6379)
+(def car-redis {:pool {} :spec {:host "localhost" :port 8379}})
+(car-appender/query-entries car-redis :debug)
+
+
+(def redis-log (str "debug:" redis-ip ":6379"))
+
+
+(def c-dist (bring-up-spots my-req 1 (cmds-dist "pool" redis-ip redis-log) :subnet my-sub-private :itype "t1.micro" :price 0.01 :key "girder" :minutes 100))
 (def r-dist (<!! c-dist))
 
-(def c-workers (bring-up-spots my-req 10 (cmds-workers 1 "pool" redis-ip) :subnet my-sub-private :itype "t1.micro" :price 0.01 :key "girder" :minutes 100))
+(def c-workers (bring-up-spots my-req 10 (cmds-workers 1 "pool" redis-ip redis-log) :subnet my-sub-private :itype "t1.micro" :price 0.01 :key "girder" :minutes 100))
 (def r-workers (<!! c-workers))
 
-(ex s-redis (cmd-job "pool" redis-ip 1 0))
+(ex s-redis (cmd-job "pool" redis-ip 1 0 redis-log))
--- src/acyclic/girder/testscripts/grid_test.clj
+++ src/acyclic/girder/testscripts/grid_test.clj
@@ -2,20 +2,20 @@
   (:require 
    [clj-ssh.ssh :as ssh]
    [clojure.core.async :as async 
-             :refer [<! >! <!! >!! timeout chan alt!! go close!]])
-  (:use [acyclic.awsutils.aws-script]))
-
+             :refer [<! >! <!! >!! timeout chan alt!! go close!]]
+   [taoensso.timbre :as timbre]
+   [taoensso.timbre.appenders.carmine :as car-appender])
+  (:use [acyclic.awstools.core]))
+(timbre/refer-timbre)
 
 ;; (def caws (first (bring-up-aws 10)))
 
 (def n 50)
 
+(slurp-ec2-info "EC2DATA.clj")
+
 (def runjar ["java" "-cp" "girder.jar" "acyclic.girder.testutils.grid" "--opts"])
 (defn download-jar [sessions] (async/map vector  (map #(ex-async  % "aws s3 cp s3://dist-ec2/girder.jar .") sessions)))
-
-
-(defn my-info (read-string (slurp EC2DATA.clj)))
-
 
 (def cmd-getjar
   "aws s3 cp s3://dist-ec2/girder.jar .\n")
@@ -32,6 +32,13 @@
 (def cr (bring-up-aws my-req 1 :udata "bin/redis-server\n" :itype "m3.medium" :price 0.03))
 (def req-redis (<!! cr))
 (def redis (first (:ips req-redis)))
+(def r)
+
+ssh/forward-remote-port
+
+(def redconn {:pool {} :spec {:host "localhost" :port 6379}})
+
+
 
 (def cw  (bring-up-aws my-req 10 :udata (cmd-workers 2 redis) :itype "t1.micro" :price 0.01))
 (def req-workers (<!! cw))
@@ -42,7 +49,7 @@
 (def sess (ssh-session (first (:hosts req-workers))))
 (ex sess (cmd-job "pool" redis 50 2))
 
-)
+
 
 
 
