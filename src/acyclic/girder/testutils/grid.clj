f7b2ff6274357b85d8a5f700add8d87ab8e8fa83
Peter Fraenkel <pnf@podsnap.com>
2014-10-15 15:21:36 -0400

    Remove redundant options.  Add docs.

--- src/acyclic/girder/testutils/grid.clj
+++ src/acyclic/girder/testutils/grid.clj
@@ -15,7 +15,7 @@
    [nil "--port PORT" "Redis port" :default 6379 :parse-fn #(Integer/parseInt %)]
    [nil "--jobmsecs MSECS" "Amount of time for the job to take" :parse-fn #(Integer/parseInt %) :default 2]
    [nil "--pool POOLID" "Pool ID" :default nil]
-   [nil "--helper MSECS" "Launch helper for the designated pool" :parse-fn #(Integer/parseInt %) :default nil]
+   [nil "--helper MSECS" "Launch helper for the designated pool (or distributor, also specified)" :parse-fn #(Integer/parseInt %) :default nil]
    [nil "--jobs N" :default 0 :parse-fn #(Integer/parseInt %)]
    [nil "--jobtimeout SECS" "Time to wait." :parse-fn #(Integer/parseInt %) :default 10]
    [nil "--reclevel N" "Number of recursions" :parse-fn #(Integer/parseInt %) :default 0]
@@ -23,7 +23,6 @@
    [nil "--cleanup"]
    ["-i" "--id NUM" "Some id number" :default 0 :parse-fn #(Integer/parseInt %)]
    ["-o" "--opts OPTS" "EDN string" :default nil :parse-fn read-string]
-   [nil "--repl" "Set when running in REPL, so exit isn't called"]
    [nil "--cmt COMMENT" "Some comment to stick into job request ids" :default (str (rand))]])
 
 (grid/cdefn recbog [msec jobnum reclevel numrecjobs args]
@@ -44,17 +43,17 @@
 (defn doit [opts]
   (acyclic.girder.grid.redis/init! (:host opts) (:port opts))
   (let [{:keys [numrecjobs reclevel cmt help pool worker distributor host port jobmsecs jobs jobtimeout helper cleanup]} opts]
-    {:cleanup
+    {:cleanup         ;; --cleanup
      (when cleanup (grid/cleanup))
-     :distributor
+     :distributor     ;; --distributor POOLID1[,POOLID2,...]
      (when distributor
        (let [ds (clojure.string/split distributor #",")]
          (for [d ds]
            (grid/launch-distributor d pool))))
-     :helper
+     :helper          ;; --helper MSECS --pool POOLID
      (when helper
-       (grid/launch-helper pool helper))
-     :worker
+       (grid/launch-helper (or distributor pool) helper))
+     :worker          ;; --worker (N-WORKERS | WID1[,WID2,...])
      (when worker
        (let [u  (java.util.UUID/randomUUID)
              n  (try (Integer/parseInt worker) (catch Exception e nil))
@@ -63,9 +62,10 @@
                   (clojure.string/split worker #","))]
          (for [w ws]
            (grid/launch-worker w pool))))
-     :jobs
+     :jobs           ;; --jobs N
      (when (and pool jobs (pos? jobs))
-       (let [c (async/map vector (map #(grid/enqueue pool [recbog jobmsecs % reclevel numrecjobs cmt]) (range jobs)))
+       (let [f (fn [i] (grid/enqueue pool [recbog jobmsecs i reclevel numrecjobs cmt] false "cli"))
+             c (async/map vector (map f (range jobs)))
              t (async/timeout (* 1000 jobtimeout))]
          (let [[v ch] (async/alts!! [c t])]
            (or v "timeout"))))}
