b48ac7d177b6cd5b0e8b58330150ea5b885fb0ef
Peter Fraenkel <pnf@podsnap.com>
2014-10-15 15:23:03 -0400

    Latest script.

--- src/acyclic/girder/testscripts/aws_test.clj
+++ src/acyclic/girder/testscripts/aws_test.clj
@@ -8,6 +8,7 @@
   (:require [clojure.tools.cli :refer [parse-opts]]
             [clojure.core.async :as async :refer [<! >! <!! >!! timeout chan alt!! go close!]]
             [taoensso.timbre :as timbre]
+            [taoensso.carmine :as car :refer (wcar)]
             [taoensso.timbre.appenders.carmine :as car-appender]
             [clj-ssh.ssh :as ssh]))
 (timbre/refer-timbre)
@@ -27,9 +28,10 @@
         " --pool " pool " --host " ip " --hang 1000"
         (addlog log))])
 
-(defn cmds-dist [pool ip & [log]]
+(defn cmds-dist [pool ip msec & [log]]
     [(cmd-getjar)
      (str "java -cp girder.jar acyclic.girder.testutils.grid --distributor " pool
+          (when (pos? msec) (str  " --helper " msec))
           " --host " ip " --hang 1000" (addlog log))])
 
 
@@ -38,12 +40,15 @@
         " --host " ip " --jobs " jobs " --reclevel " reclevel
          (addlog log)))
 
-(defn cmds-redis [] [(str "bin/redis-server")])
+(defn cmds-redis [] [(cmd-getjar)
+                     (str "bin/redis-server")])
 
+
+(slurp-ec2-info "EC2DATA.clj")
 
 (def c-listener (start-up-listener))
 
-(slurp-ec2-info "EC2DATA.clj")
+
 
 (def c-nat (bring-up-instances [(:nat-id my-ec2-info)]))
 (def c-redis  (bring-up-spots my-req 1 (cmds-redis) :subnet my-sub-public :itype "m3.medium" :price 0.05 :key "telekhine" :log "debug"))
@@ -52,24 +57,22 @@
 
 (def redis (first (vals r-redis)))
 (def redis-ip (:ip redis))
+(def redis-log (str "debug:" redis-ip ":6379"))
 (def redis-host (:host redis))
 (def s-redis (ssh-session redis-host))
 (ex s-redis "bin/redis-cli flushdb")
 
 
-(ssh/connect s-redis)
+;;(ssh/connect s-redis)
 (ssh/forward-local-port s-redis 8379 6379)
 (def car-redis {:pool {} :spec {:host "localhost" :port 8379}})
 (car-appender/query-entries car-redis :debug)
 
 
-(def redis-log (str "debug:" redis-ip ":6379"))
-
-
-(def c-dist (bring-up-spots my-req 1 (cmds-dist "pool" redis-ip redis-log) :subnet my-sub-private :itype "t1.micro" :price 0.01 :key "girder" :minutes 100))
-(def r-dist (<!! c-dist))
-
+(def c-dist (bring-up-spots my-req 1 (cmds-dist "pool" redis-ip 100 redis-log) :subnet my-sub-private :itype "t1.micro" :price 0.01 :key "girder" :minutes 100))
 (def c-workers (bring-up-spots my-req 10 (cmds-workers 1 "pool" redis-ip redis-log) :subnet my-sub-private :itype "t1.micro" :price 0.01 :key "girder" :minutes 100))
+
+(def r-dist (<!! c-dist))
 (def r-workers (<!! c-workers))
 
 (ex s-redis (cmd-job "pool" redis-ip 1 0 redis-log))
